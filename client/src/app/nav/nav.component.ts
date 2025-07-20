f5a97b0fa92c6b7321b1cfea5c073f3af9c55b81
diokats <dioktys@gmail.com>
2025-07-20 19:59:03 +0300

    nav-links angular route bootsrap theme

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,26 +2,32 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
+
   model: any = {};
-  loggedIn = false;
+  // loggedIn = false;
 
   login() {
     // Implement login logic here
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: _ => {
+        this.router.navigateByUrl('/members');
       },
-      error: error =>console.log(error)
+      error: error => this.toastr.error(error.error)
     });
   }
 
@@ -29,6 +35,7 @@ export class NavComponent {
     // Implement logout logic here
     // this.accountService.logout();
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
